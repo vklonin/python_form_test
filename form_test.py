858fa3d43c2739cd589a00f7eb9536ec82472bce
vladimir klonin <vklonin@gmail.com>
2022-06-28 18:45:02 +0400

    added new test for a table

--- form_test.py
+++ form_test.py
@@ -4,8 +4,10 @@ from selene import be, have
 
 def test_of_student_form():
 
-    # page object
+
     browser.open('https://demoqa.com/automation-practice-form')
+
+    # page object
     first_name = browser.element("#firstName")
     last_name = browser.element("#lastName")
     user_email = browser.element("#userEmail")
@@ -66,3 +68,71 @@ def test_of_student_form():
     for row, validation_row in zip(table_rows, validation_pairs):
         assert row.elements('td').element(0).text == validation_row[0]
         assert row.elements('td').element(1).text == validation_row[1]
+
+def test_of_webtables():
+
+    browser.open('https://demoqa.com/webtables')
+    # page object
+    table = browser.element(".rt-table")
+    new_button = browser.element("#addNewRecordButton")
+
+    edit_record_2_button = browser.element("#edit-record-2")
+    delete_record_3_button = browser.element("#delete-record-3")
+    modal_form_first_name =  browser.element("#firstName")
+    modal_form_last_name = browser.element("#lastName")
+    modal_form_user_email = browser.element("#userEmail")
+    modal_form_age = browser.element("#age")
+    modal_form_salary = browser.element("#salary")
+    modal_form_department = browser.element("#department")
+
+    # adding a new line
+    new_button.click()
+    modal_form_first_name.set_value("Ranga")
+    modal_form_last_name.set_value("Caranam")
+    modal_form_user_email.set_value("Ranga@Caranam.com")
+    modal_form_age.set_value("25")
+    modal_form_salary.set_value("100")
+    modal_form_department.set_value("accounting").press_tab().press_enter()
+
+    # validation that a line is added
+    forth_line = table.elements(".rt-tr-group").element(3).elements(".rt-td")
+    assert forth_line.element(0).text == "Ranga"
+    assert forth_line.element(1).text == "Caranam"
+    assert forth_line.element(2).text == "25"
+    assert forth_line.element(3).text == "Ranga@Caranam.com"
+    assert forth_line.element(4).text == "100"
+    assert forth_line.element(5).text == "accounting"
+
+    # editing a second line
+    edit_record_2_button.click()
+    modal_form_first_name.set_value("Ranga")
+    modal_form_last_name.set_value("CaranamJr")
+    modal_form_user_email.set_value("RangaJr@Caranam.com")
+    modal_form_age.set_value("24")
+    modal_form_salary.set_value("99")
+    modal_form_department.set_value("accounting").press_tab().press_enter()
+
+    # validation that a line is edited
+    second_line = table.elements(".rt-tr-group").element(1).elements(".rt-td")
+    assert second_line.element(0).text == "Ranga"
+    assert second_line.element(1).text == "CaranamJr"
+    assert second_line.element(2).text == "24"
+    assert second_line.element(3).text == "RangaJr@Caranam.com"
+    assert second_line.element(4).text == "99"
+    assert second_line.element(5).text == "accounting"
+    
+    # deleting a tird line
+    delete_record_3_button.click()
+    # validation that third line is deleted
+    third_line = table.elements(".rt-tr-group").element(2).elements(".rt-td")
+    assert third_line.element(0).text == "Ranga"
+    assert third_line.element(1).text == "Caranam"
+    assert third_line.element(2).text == "25"
+    assert third_line.element(3).text == "Ranga@Caranam.com"
+    assert third_line.element(4).text == "100"
+    assert third_line.element(5).text == "accounting"
+
+
+
+
+
